abc8ef7eb5e3222fa5c06da651d733a92f19d800
Matt Groth <mgroth49@gmail.com>
2021-05-19 02:37:53 -0400

    Merge branch 'master' of https://github.com/mgroth0/gui
    # Conflicts:
    #	src/main/kotlin/matt/gui/proto/protonodes.kt

---
+++ KJ/v1/src/main/kotlin/matt/v1/compcache/CompAndCache.kt
@@ -0,0 +1,316 @@
+@file:Suppress("ProtectedInFinal")
+
+package matt.v1.compcache
+
+import matt.kjlib.jmath.BIG_E
+import matt.kjlib.jmath.e
+import matt.kjlib.jmath.simpleFactorial
+import matt.kjlib.str.tab
+import matt.kjlib.stream.onEveryIndexed
+import matt.klib.dmap.withStoringDefault
+import matt.klib.math.sq
+import matt.klib.ranges.step
+import matt.v1.lab.Experiment.CoreLoop
+import matt.v1.lab.allComplexCells
+import matt.v1.model.ComplexCell
+import matt.v1.model.PopulationResponse
+import matt.v1.model.Stimulus
+import org.apache.commons.math3.fitting.GaussianCurveFitter
+import org.apache.commons.math3.fitting.WeightedObservedPoint
+import org.apfloat.Apfloat
+import org.apfloat.ApfloatMath
+import java.math.BigDecimal
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+abstract class ComputeCache<I, O> {
+  val disableCache = false
+  val computeCache = mutableMapOf<I, O>().withStoringDefault {
+	compute(it)
+  }
+  abstract val compute: I.()->O
+}
+
+abstract class ComputeInput<I, O> {
+  protected abstract val computer: ComputeCache<I, O>
+  fun findOrCompute(debug: Boolean = false): O {
+	return if (!debug) {
+	  if (computer.disableCache) {
+		computer.compute.invoke(this as I)
+	  } else computer.computeCache[this as I]!!
+	} else {
+	  println("DEBUG:${this}")
+	  tab("disableCache=${computer.disableCache}")
+	  tab("in=${(this as I) in computer.computeCache}")
+	  val r = if (computer.disableCache) {
+		computer.compute.invoke(this as I)
+	  } else computer.computeCache[this]!!
+	  tab("r=${r}")
+	  r
+	}
+  }
+}
+
+data class Norm(
+  val dX: Double,
+  val dY: Double,
+): ComputeInput<Norm, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<Norm, Double>() {
+	override val compute: Norm.()->Double = { sqrt(dX.sq() + dY.sq()) }
+  }
+}
+
+data class Weight(
+  val norm: Double,
+  val sigmaPool: Double,
+): ComputeInput<Weight, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<Weight, Double>() {
+	override val compute: Weight.()->Double = {
+	  e.pow((-(norm.sq()))/(2*sigmaPool.sq()))
+	}
+  }
+}
+
+data class DivNorm(
+  val D: Double?,
+  val c: Double, /*Suppressive Field Gain*/
+  val v: Double, /*SemiSaturation Constant*/
+  val S: Double? /*Suppressive Field*/
+): ComputeInput<DivNorm, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<DivNorm, Double>() {
+	override val compute: DivNorm.()->Double = {
+	  D!!/(v + c*S!!)
+	}
+  }
+}
+
+data class AttentionAmp(
+  val norm: Norm
+): ComputeInput<AttentionAmp, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<AttentionAmp, Double>() {
+	private const val G = 7.0
+	private const val SIGMA_ATTENTION = 2.0
+	override val compute: AttentionAmp.()->Double = {
+	  1 + G*e.pow(-((norm.findOrCompute().sq())/(2*SIGMA_ATTENTION.sq())))
+	}
+  }
+}
+
+data class BayesianPriorC(
+  val c0: Double,
+  val w0: Double,
+  val t: Double
+): ComputeInput<BayesianPriorC, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<BayesianPriorC, Double>() {
+	override val compute: BayesianPriorC.()->Double = {
+	  val v = c0 - w0*cos(4*t*PI/180)
+	  require(v >= 0)
+	  v
+	}
+  }
+}
+
+data class PPCUnit(
+  val ft: Double,
+  val ri: /*Double*/Int
+): ComputeInput<PPCUnit, Double>() {
+  override val computer = Companion
+
+  protected companion object: ComputeCache<PPCUnit, Double>() {
+	override val compute: PPCUnit.()->Double = {
+	  val debugE = Apfloat(BIG_E)/*.alsoPrintln { "debugE:${this}" }*/
+	  val debugNft = Apfloat(BigDecimal(-ft))/*.alsoPrintln { "debugNft:${this}" }*/
+	  val debugTopLeft = ApfloatMath.pow(debugE, debugNft)/*.alsoPrintln { "debugTopLeft:${this}" }*/
+	  /*println("debugTopRight-ft=${ft}")
+	  println("debugTopRight-ri=${ri}")*/
+	  val beforeBD = ft.pow(ri)
+	  /*println("beforeBD=${beforeBD}")*/
+	  val debugTopRight = Apfloat(BigDecimal(beforeBD))/*.alsoPrintln { "debugTopRight:${this}" }*/
+
+	  val debugBottom =
+		  Apfloat(
+			ri
+				/*.generalizedFactorialOrSimpleIfInfOrNaN()*/
+				.simpleFactorial()
+				/*.alsoPrintln { "ri:${ri},Fact:${this}" }*/
+			/*simpleFactorial()*/
+		  )/*.alsoPrintln { "debugBottom:${this}" }*/
+	  (
+		  (
+			  debugTopLeft
+				  .multiply(
+					debugTopRight
+				  ))
+			  .divide(
+				debugBottom
+			  )
+	  )
+		  .toDouble()
+	}
+  }
+}
+
+data class PreDNPopR(
+  val stim: Stimulus,
+  val attention: Boolean
+): ComputeInput<PreDNPopR, PopulationResponse>() {
+  override val computer = Companion
+
+
+  companion object: ComputeCache<PreDNPopR, PopulationResponse>() {
+	lateinit var coreLoopForStatusUpdates: CoreLoop
+	override val compute: PreDNPopR.()->PopulationResponse = {
+	  allComplexCells
+		  .asSequence()
+		  .onEveryIndexed(10) { i, _ -> coreLoopForStatusUpdates.update(i = i) }
+		  .associateWith {
+			it.stimulate(
+			  stim,
+			  attention = attention
+			)
+		  }.let {
+			PopulationResponse(
+			  m = it
+			)
+		  }
+	}
+  }
+}
+
+data class Stimulation(
+  val cell: ComplexCell,
+  val stim: Stimulus,
+  val popR: PopulationResponse?,
+  val attention: Boolean
+): ComputeInput<Stimulation, Double>() {
+  override val computer = Companion
+
+
+  companion object: ComputeCache<Stimulation, Double>() {
+	override val compute: Stimulation.()->Double = {
+	  cell.stimulate(
+		stim = stim,
+		attention = attention,
+		popR = popR
+	  )
+	}
+  }
+}
+
+data class XTheta(
+  val t: Double,
+  val dX: Double,
+  val dY: Double
+): ComputeInput<XTheta, Double>() {
+  override val computer = Companion
+
+
+  companion object: ComputeCache<XTheta, Double>() {
+	override val compute: XTheta.()->Double = {
+	  (cos(t)*(dX) + sin(t)*(dY))
+	}
+  }
+}
+
+data class YTheta(
+  val t: Double,
+  val dX: Double,
+  val dY: Double
+): ComputeInput<YTheta, Double>() {
+  override val computer = Companion
+
+
+  companion object: ComputeCache<YTheta, Double>() {
+	override val compute: YTheta.()->Double = {
+	  (-sin(t)*(dX) + cos(t)*(dY))
+	}
+  }
+}
+
+data class GaussianCoef(
+  val a: Double,
+  val b: Double,
+  val c: Double
+)
+
+data class GaussianCoefCalculator(
+  val points: List<Point>
+): ComputeInput<GaussianCoefCalculator, GaussianCoef>() {
+  override val computer = Companion
+
+  companion object: ComputeCache<GaussianCoefCalculator, GaussianCoef>() {
+	val fitter by lazy { GaussianCurveFitter.create() }
+	override val compute: GaussianCoefCalculator.()->GaussianCoef = {
+	  fitter.fit(points.map { WeightedObservedPoint(1.0, it.x, it.y) }).let {
+		GaussianCoef(
+		  a = it[0],
+		  b = it[1],
+		  c = it[2]
+		)
+	  }
+	}
+  }
+}
+
+
+data class Point(val x: Double, val y: Double)
+data class GaussianFit(
+  val g: GaussianCoef,
+  val xMin: Double,
+  val xStep: Double,
+  val xMax: Double
+): ComputeInput<GaussianFit, List<Point>>() {
+
+  constructor(
+	points: List<Point>,
+	xMin: Double,
+	xStep: Double,
+	xMax: Double
+  ): this(GaussianCoefCalculator(points).findOrCompute(), xMin = xMin, xStep = xStep, xMax = xMax)
+
+  override val computer = Companion
+
+
+  companion object: ComputeCache<GaussianFit, List<Point>>() {
+	override val compute: GaussianFit.()->List<Point> = {
+	  val range = (xMin..xMax step xStep).toList()
+	  val array = mutableListOf<Point>()
+	  range.forEach { x ->
+		array += Point(
+		  x = x,
+		  y = GaussianPoint(a = g.a, b = g.b, c = g.c, x = x).findOrCompute()
+		)
+	  }
+	  array
+	}
+  }
+}
+
+data class GaussianPoint(
+  val a: Double,
+  val b: Double,
+  val c: Double,
+  val x: Double
+): ComputeInput<GaussianPoint, Double>() {
+  override val computer = Companion
+
+
+  companion object: ComputeCache<GaussianPoint, Double>() {
+	override val compute: GaussianPoint.()->Double = {
+	  a*e.pow(-(x - b).sq()/(2*c.sq()))
+	}
+  }
+}
---
+++ KJ/v1/src/main/kotlin/matt/v1/exps/experiments.kt
@@ -0,0 +1,474 @@
+package matt.v1.exps
+
+import matt.v1.gui.Figure
+import matt.v1.gui.StatusLabel
+import matt.v1.lab.ExpCategory.OTHER
+import matt.v1.lab.ExpCategory.ROSENBERG
+import matt.v1.lab.Experiment
+import matt.v1.lab.Experiment.XVar.CONTRAST
+import matt.v1.lab.Experiment.XVar.DIST_4_ATTENTION
+import matt.v1.lab.Experiment.XVar.FT
+import matt.v1.lab.Experiment.XVar.MASK
+import matt.v1.lab.Experiment.XVar.PREF_ORIENTATION
+import matt.v1.lab.Experiment.XVar.REL_STIM_ORIENTATION
+import matt.v1.lab.Experiment.XVar.SIZE
+import matt.v1.lab.Experiment.XVar.STIM_AND_PREF_ORIENTATION
+import matt.v1.lab.Experiment.XVar.STIM_ORIENTATION
+import matt.v1.lab.Fit.Gaussian
+import matt.v1.lab.PoissonVar.FAKE1
+import matt.v1.lab.PoissonVar.FAKE10
+import matt.v1.lab.PoissonVar.FAKE5
+import matt.v1.lab.PoissonVar.YES
+import matt.v1.lab.SeriesCfg
+import matt.v1.lab.YExtract.CCW
+import matt.v1.lab.YExtract.MAX_PPC
+import matt.v1.lab.YExtract.POP_GAIN
+import matt.v1.lab.YExtract.PPC
+import matt.v1.lab.YExtract.PPC_UNIT
+import matt.v1.lab.YExtract.PRIOR
+import matt.v1.lab.rCfg
+import matt.v1.model.ASD_SIGMA_POOLING
+import matt.v1.model.ATTENTION_SUPP_SIGMA_POOLING
+import matt.v1.model.tdDivNorm
+import kotlin.math.pow
+
+/*(Rosenberg et al. 2015)*/
+/*(Rosenberg et al. 2015 SI)*/
+
+fun experiments(fig: Figure, statusLabel: StatusLabel): List<Experiment> {
+
+
+  val noDN = SeriesCfg(label = "- D.N.")
+  val withDN = noDN.copy(label = "+ D.N.", DN = true)
+  val baseExp = Experiment(
+	name = "3.B",
+	title = "DN causes saturation with ↑ contrast",
+	xlabel = "% Contrast",
+	ylabel = "Neural Response % Maximum",
+	fig = fig,
+	statusLabel = statusLabel,
+	xVar = CONTRAST,
+	xStep = rCfg.F3B_STEP,
+	series = listOf(noDN, withDN),
+	normToMaxes = true,
+	category = ROSENBERG
+  )
+
+
+  val exps = mutableListOf(baseExp)
+  exps += baseExp.copy(
+	name = "3.C",
+	title = "DN allows suppression",
+	xlabel = "Mask % Contrast",
+	xMax = 50.0,
+	xVar = MASK,
+	xStep = rCfg.F3C_STEP
+  )
+  exps += baseExp.copy(
+	name = "3.D",
+	title = "DN causes tuning curve to peak at optimal size",
+	xlabel = "Size (Degrees)",
+	xMax = 10.0,
+	xVar = SIZE,
+	xStep = rCfg.F3D_STEP,
+	xMin = rCfg.F3D_STEP,
+	baseContrast = 1.0
+  )
+
+  val td = withDN.copy(label = "TD")
+  val ascC = td.copy(label = "ASC c", divNorm = tdDivNorm.copy(c = 7.5*10.0.pow(-5)))
+  val popGainBySize = exps.last().copy(
+	name = "4.C",
+	title = "↓ D.N. in ASD causes ↑ the absolute pop. gain (matches b. data)",
+	xMax = 6.05,
+	xMin = 1.55,
+	series = listOf(td, ascC),
+	ylabel = "Population Gain",
+	autoY = true,
+	normToMaxes = false
+  )
+  exps += popGainBySize
+  val contrastSensitivity = exps.last().copy(
+	name = "4.D",
+	title = "pop. gain from ↓ D.N. in ASD diverges from TD as contrast ↑ (matches b. data)",
+	xMin = 0.0,
+	xMax = 100.0,
+	xVar = CONTRAST,
+	xlabel = baseExp.xlabel,
+	xStep = rCfg.F3B_STEP
+  )
+  exps += contrastSensitivity
+
+  exps += baseExp.copy(
+	name = "5.C",
+	title = "dist. from center of attn. causes ↓ pop. gain and then ↑ (matches b. data)",
+	xMin = 0.0,
+	xMax = 10.0,
+	xVar = DIST_4_ATTENTION,
+	xStep = rCfg.F5C_STEP,
+	xlabel = "Distance (Degrees)",
+	ylabel = exps.last().ylabel,
+	troughShift = true,
+	autoY = true,
+	series = listOf(td, ascC),
+	baseContrast = 1.0,
+	normToMaxes = false
+  )
+  exps += exps.last().copy(
+	name = "5.D",
+	title = "pop. gain gradient from attn increases with ASD symptomatology (↑c) (matches b. data)",
+	xMetaMin = 0.0,
+	xMetaMax = 50.0,
+	xlabel = "Gain Term % Decrease",
+	ylabel = "Population Gain Gradient",
+	metaGain = true,
+	series = listOf(td) /*TD gets modified along AQ scale*/
+  )
+
+  val contrast1 =
+	  td.copy(label = "${Experiment.CONTRAST1}%", contrastAlpha = Experiment.CONTRAST1*0.01)
+  val contrast2 =
+	  td.copy(label = "${Experiment.CONTRAST2}%", contrastAlpha = Experiment.CONTRAST2*0.01)
+
+  exps += baseExp.copy(
+	name = "S1.A",
+	xVar = PREF_ORIENTATION,
+	xMin = 0.0,
+	xMax = 179.0,
+	xStep = rCfg.FS1_STEP,
+	stimTrans = { copy(SF = 3.0, f = f.copy(t = 90.0)) },
+	xlabel = "Preferred Orientation (Degrees)",
+	ylabel = "Neural Response",
+	autoY = true,
+	series = listOf(contrast1, contrast2),
+	title = "(strange) definition of pop. gain",
+	normToMaxes = false
+	/*alpha is 0.075 or 0.2*/
+	/*theta ranges from 0 to 180 in 0.25 steps*/
+  )
+  exps += exps.last().copy(
+	name = "S1.B",
+	xVar = STIM_ORIENTATION,
+	xlabel = "Stimulus Orientation (Degrees)",
+	ylabel = "p(Θ|r)",
+	autoY = true,
+	title = "Probabilistic Population Code (PPC)",
+	stimTrans = { copy(SF = 3.0) },
+	series = exps.last().series.map { it.copy(yExtract = PPC) } + listOf(
+	  contrast1.copy(label = "${contrast1.label} (Poisson)", poissonVar = YES, yExtract = PPC),
+	  contrast2.copy(label = "${contrast2.label} (Poisson)", poissonVar = YES, yExtract = PPC)
+	)
+	/*alpha is 0.075 or 0.2*/
+  )
+
+  val ascV = td.copy(label = "ASC v", divNorm = tdDivNorm.copy(v = 0.01))
+  val ascSS = td.copy(label = "ASC σs", sigmaPooling = ASD_SIGMA_POOLING)
+
+  exps += popGainBySize.copy(
+	name = "S2.A",
+	title = "spatial suppression across ASD models",
+	series = listOf(
+	  ascC,
+	  ascSS,
+	  ascV,
+	  td
+	),
+  )
+  exps += contrastSensitivity.copy(
+	name = "S2.B",
+	title = "contrast sensitivity across ASD models",
+	series = listOf(ascC, ascSS, ascV, td),
+  )
+  exps += exps.first { it.name == "5.C" }.copy(
+	name = "S3.B",
+	title = "5.C with low contrast",
+	baseContrast = 0.1,
+	troughShift = false
+  )
+  exps += exps.first { it.name == "5.D" }.copy(
+	name = "S3.C",
+	title = "5.D with low contrast",
+	baseContrast = 0.1
+  )
+  exps += exps.first { it.name == "5.C" }.copy(
+	name = "S3.D",
+	title = "5.C with ↑D.N. pool size",
+	troughShift = true,
+	series = listOf(
+	  td,
+	  td.copy(label = "Increased σs", sigmaPooling = ATTENTION_SUPP_SIGMA_POOLING)
+	),
+  )
+  exps += exps.first { it.name == "5.C" }.copy(
+	name = "S3.E",
+	title = "5.C across v (semisaturation constant) changes",
+	troughShift = false,
+	series = listOf(
+	  td.copy(label = "TD V1") /*same as TD with different label*/,
+	  td.copy(label = "v = 20000 (2 in paper)", divNorm = tdDivNorm.copy(v = 20_000.0)),
+	  td.copy(label = "v = 40000 (4 in paper)", divNorm = tdDivNorm.copy(v = 40_000.0)),
+	  td.copy(label = "v = 60000 (6 in paper)", divNorm = tdDivNorm.copy(v = 60_000.0))
+	),
+  )
+
+
+  val flatPrior = td.copy(
+	label = "Flat",
+	priorWeight = 0.0,
+	yExtract = PRIOR,
+	divNorm = td.divNorm.copy(v = 0.000001) /*DEBUG*/
+  )
+
+  val weakPrior = flatPrior.copy(
+	label = "Weak",
+	priorWeight = 2.5*10.0.pow(-5)/* 7*10.0.pow(-5) *//*DEBUG*/ /**/
+  )
+  val strongPrior = flatPrior.copy(
+	label = "Strong",
+	priorWeight = 5*10.0.pow(-5)  /*7.49*10.0.pow(-5)*/
+  )
+  val veryStrongPrior = 7.5*10.0.pow(-5)
+  exps += baseExp.copy(
+	name = "S4.A",
+	title = "Bayesian Priors: Suppressive Field Gain Term",
+	xMin = 0.0,
+	xMax = 170.0,
+	xStep = 1.0,
+	xVar = PREF_ORIENTATION,
+	xlabel = "Preferred Orientation (Degrees)",
+	ylabel = "c(Θ)",
+	autoY = true,
+	series = listOf(flatPrior, weakPrior, strongPrior),
+	normToMaxes = false,
+  )
+
+
+  val flatPriorR = flatPrior.copy(
+	label = "Flat (45° stim)",
+	yExtract = POP_GAIN,
+	stimTheta = 45.0,
+	contrastAlpha = 0.1,
+	stimGaussianEnveloped = false,
+	stimSF = 3.0
+  )
+  exps += exps.last().copy(
+	name = "S4.B",
+	title = "Bayesian Priors: Oblique effect is diminished in Autism",
+	ylabel = "Neural Response",
+	xStep = 15.0,
+	series = listOf(
+	  flatPriorR,
+	  flatPriorR.copy(
+		label = "Weak (45° stim)",
+		priorWeight = weakPrior.priorWeight
+	  ),
+	  flatPriorR.copy(
+		label = "Strong (45° stim)",
+		priorWeight = strongPrior.priorWeight
+	  ),
+	  flatPriorR.copy(
+		label = "Flat (90° stim)",
+		stimTheta = 90.0,
+		priorWeight = flatPriorR.priorWeight
+	  ),
+	  flatPriorR.copy(
+		label = "Weak (90° stim)",
+		stimTheta = 90.0,
+		priorWeight = weakPrior.priorWeight
+	  ),
+	  flatPriorR.copy(
+		label = "Strong (90° stim)",
+		stimTheta = 90.0,
+		priorWeight = strongPrior.priorWeight
+	  )
+	)
+  )
+
+  val flatPriorPPC = flatPriorR.copy(label = "Flat", yExtract = MAX_PPC, stimTheta = null)
+  exps += exps.last().copy(
+	name = "S4.C.1",
+	title = "Bayesian Priors: Oblique effect is diminished in Autism (PPCs)", /*Posterior Probability Distributions*/
+	ylabel = "Max p(Θ|r)",
+	xlabel = "Stimulus Orientation",
+	xVar = STIM_ORIENTATION,
+	series = listOf(
+	  flatPriorPPC,
+	  flatPriorPPC.copy(
+		label = "Weak",
+		priorWeight = weakPrior.priorWeight,
+	  ),
+	  flatPriorPPC.copy(
+		label = "Strong",
+		priorWeight = strongPrior.priorWeight
+	  )
+	)
+  )
+
+  exps += exps.last().copy(
+	name = "S4.C.2",
+	series = listOf(
+	  flatPriorPPC.copy(
+		label = "Flat (Poisson)",
+		poissonVar = YES
+	  ),
+	  flatPriorPPC.copy(
+		label = "Weak (Poisson)",
+		priorWeight = weakPrior.priorWeight,
+		poissonVar = YES
+	  ),
+	  flatPriorPPC.copy(
+		label = "Strong (Poisson)",
+		priorWeight = strongPrior.priorWeight,
+		poissonVar = YES
+	  ),
+	)
+  )
+  exps += exps.last().copy(
+	name = "S4.C.3",
+	series = listOf(
+	  flatPriorPPC.copy(
+		label = "Flat (Fake1 Poisson)",
+		poissonVar = FAKE1
+	  ),
+	  flatPriorPPC.copy(
+		label = "Weak (Fake1 Poisson)",
+		priorWeight = weakPrior.priorWeight,
+		poissonVar = FAKE1
+	  ),
+	  flatPriorPPC.copy(
+		label = "Strong (Fake1 Poisson)",
+		priorWeight = strongPrior.priorWeight,
+		poissonVar = FAKE1
+	  )
+	)
+  )
+  exps += exps.last().copy(
+	name = "S4.C.4",
+	series = listOf(
+	  flatPriorPPC.copy(
+		label = "Flat (Fake10 Poisson)",
+		poissonVar = FAKE10
+	  ),
+	  flatPriorPPC.copy(
+		label = "Weak (Fake10 Poisson)",
+		priorWeight = weakPrior.priorWeight,
+		poissonVar = FAKE10
+	  ),
+	  flatPriorPPC.copy(
+		label = "Strong (Fake10 Poisson)",
+		priorWeight = strongPrior.priorWeight,
+		poissonVar = FAKE10
+	  )
+	)
+  )
+
+  exps += exps.last().copy(
+	name = "S4.D",
+	ylabel = "Baseline Activity",
+	xlabel = "Preferred Orientation (Degrees)",
+	title = "Bayesian Priors: Oblique effect is diminished in Autism (Pop. Gain)",
+	xVar = STIM_AND_PREF_ORIENTATION,
+	series = listOf(
+	  flatPriorPPC.copy(
+		label = "Baseline Activity",
+		yExtract = POP_GAIN,
+		priorWeight = strongPrior.priorWeight
+	  )
+	)
+  )
+
+  exps += baseExp.copy(
+	name = "S5.1",
+	xlabel = "Relative Orientation (Degrees)",
+	ylabel = "Population CCW Response",
+	title = "Psychometric Simulation of the Oblique Effect (Deterministic)",
+	xMin = -10.0,
+	xMax = 10.0,
+	xVar = REL_STIM_ORIENTATION,
+	xStep = 1.0,
+	series = listOf(
+	  flatPriorPPC.copy(
+		label = "90°",
+		priorWeight = veryStrongPrior,
+		yExtract = CCW,
+		relThetaMid = 90.0,
+		line = true, /*while im waiting for fixes*/
+		markers = true,
+		fit = Gaussian
+	  ),
+	  flatPriorPPC.copy(
+		label = "45°",
+		priorWeight = veryStrongPrior,
+		yExtract = CCW,
+		relThetaMid = 45.0,
+		line = true, /*while im waiting for fixes*/
+		markers = true,
+		fit = Gaussian
+	  )
+	),
+	normToMaxes = false,
+	autoY = true,
+  )
+
+  exps += exps.last().copy(
+	name = "S5.2",
+	title = exps.last().title.replace("Deterministic", "Poisson"),
+	series = exps.last().series.map {
+	  it.copy(
+		poissonVar = YES
+	  )
+	}
+  )
+  exps += exps.last().copy(
+	name = "S5.3",
+	title = exps.last().title.replace("Poisson", "Fake1 Poisson"),
+	series = exps.last().series.map {
+	  it.copy(
+		poissonVar = FAKE1
+	  )
+	}
+  )
+  exps += exps.last().copy(
+	name = "S5.4",
+	title = exps.last().title.replace("Fake1 Poisson", "Fake5 Poisson"),
+	series = exps.last().series.map {
+	  it.copy(
+		poissonVar = FAKE5
+	  )
+	}
+  )
+  exps += exps.last().copy(
+	name = "S5.5",
+	title = exps.last().title.replace("Fake5 Poisson", "Fake10 Poisson"),
+	series = exps.last().series.map {
+	  it.copy(
+		poissonVar = FAKE10
+	  )
+	}
+  )
+
+  val p0 = SeriesCfg(label = "P=0", yExtract = PPC_UNIT, poissonVarI = 0)
+  exps += baseExp.copy(
+	name = "Ideal Poisson",
+	title = "Ideal Neuronal Responses for PPCs",
+	xlabel = "f(Θ):Mean Neural Response",
+	ylabel = "PPC Unit Probability (informativeness)",
+	autoY = true,
+	normToMaxes = false,
+	category = OTHER,
+	xMin = 0.0,
+	xMax = 5.0,
+	xStep = 0.025,
+	xVar = FT,
+	series = listOf(
+	  p0,
+	  p0.copy(label = "P=1", poissonVarI = 1),
+	  p0.copy(label = "P=2", poissonVarI = 2),
+	  p0.copy(label = "P=3", poissonVarI = 3),
+	  p0.copy(label = "P=4", poissonVarI = 4),
+	)
+  )
+
+  return exps
+}
+
