9d0b5f43ffe895fe788322f0759bda5015b29e77
Matt Groth <mgroth49@gmail.com>
2021-05-27 00:37:13 -0400

    autocommit

--- KJ/v1/build.gradle.kts
+++ KJ/v1/build.gradle.kts
@@ -2,13 +2,17 @@ dependencies {
   implementation(projects.kj.gui)
   implementation(libs.bundles.multik.full)
   /*implementation(libs.aparapi)*/
-  implementation(
-	files(
-	  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi-natives.jar",
-	  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi.jar",
-	  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/bcel-6.0.jar"
+  if (isMac) {
+	implementation(
+	  files(
+		"/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi-natives.jar",
+		"/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi.jar",
+		"/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/bcel-6.0.jar"
+	  )
 	)
-  )
+  } else {
+	implementation(libs.aparapi)
+  }
   implementation(libs.commons.math)
   implementation(libs.chartFX)
   implementation(libs.apfloat)
