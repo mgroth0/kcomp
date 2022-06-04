3240820cd0737a87c6545a1db86947a9402f07cc
mgroth0 <mgroth49@gmail.com>
2022-06-04 11:54:37 -0400

    autocommit

--- KJ/v1/build.gradle.kts
+++ KJ/v1/build.gradle.kts
@@ -1,31 +1,9 @@
 dependencies {
   implementation(projects.kj.gui)
   implementation(libs.bundles.multik.full)
-  /*implementation(libs.aparapi)*/
   implementation(libs.mat)
   api(projects.kj.caching)
   api(projects.kj.async)
-  /*println("testing if newMac")*/
-  /*    if (isNewMac) {
-		  println("ok this is working...")
-		  implementation(
-				  files(
-						  "/Users/matthewgroth/registered/kcomp/KJ/jar/aparapi-natives.jar",
-						  "/Users/matthewgroth/registered/kcomp/KJ/jar/aparapi.jar",
-						  "/Users/matthewgroth/registered/kcomp/KJ/jar/bcel-6.0.jar"
-				  )
-		  )
-	  } else if (isMac) {
-		  implementation(
-				  files(
-						  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi-natives.jar",
-						  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/aparapi.jar",
-						  "/Users/matt/Desktop/registered/todo/kcomp/KJ/jar/bcel-6.0.jar"
-				  )
-		  )
-	  } else {*/
-  implementation(libs.aparapi)
-  /*}*/
   implementation(libs.commons.math)
   implementation(libs.chartFX)
   implementation(libs.apfloat)
@@ -34,31 +12,7 @@ dependencies {
   implementation(projects.kj.fxlib.console)
   implementation(projects.kj.dataman)
   implementation(projects.kj.kjlib.jmath)
-
-  /*implementation("org.tensorflow:tensorflow-core-api:0.4.0")*/
-  /*implementation("org.tensorflow:tensorflow-core-platform:0.4.0")*/
-  /*implementation(
-	files(
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-api-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-api-0.4.0-SNAPSHOT-macosx-arm64.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-generator-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-core-platform-0.4.0-SNAPSHOT.jar",
-	  "/Users/matthewgroth/registered/kcomp/KJ/jar/tensorflow-framework-0.4.0-SNAPSHOT.jar",
-	)
-  )*/
-
-
 }
-
-/*
-configurations.all {
-  resolutionStrategy.dependencySubstitution {
-    substitute(module("org.tensorflow:tensorflow-core-api"))
-      .using(module("org.tensorflow:tensorflow-core-api:0.4.0"))
-      .withClassifier("macosx-x86_64")
-  }
-}*/
-
 
 plugins {
   kotlin("plugin.serialization")
