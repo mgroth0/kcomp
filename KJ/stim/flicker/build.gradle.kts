cfab5c2a681893603cc5395d2aecf7512162bdb1
mgroth0 <mgroth49@gmail.com>
2022-06-10 14:40:15 -0400

    autocommit

--- KJ/stim/flicker/build.gradle.kts
+++ KJ/stim/flicker/build.gradle.kts
@@ -3,6 +3,7 @@ modtype = APP
 
 dependencies {
   implementation(projects.kj.gui)
+  implementation(libs.fx.media)
 //  implementation(projects.kj.fx.media)
 //  implementation(projects.kj.async)
   implementation(projects.kj.fx.graphics)
