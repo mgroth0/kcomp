eb63b3a56b06c3d9ff0d6389901e5a901f0e582f
mgroth0 <mgroth49@gmail.com>
2022-06-11 15:14:22 -0400

    autocommit

--- k/sempart/src/commonMain/kotlin/matt/sempart/sempart.kt
+++ k/sempart/src/commonMain/kotlin/matt/sempart/sempart.kt
@@ -7,5 +7,5 @@ data class ExperimentData(
   val responses: Map<String, String>,
   /*val segment: String,
   val label: String,*/
-  val trialLog: List<List<Pair<Long, String>>>,
+  val trialLog: List<Pair<Long, String>>,
 )
