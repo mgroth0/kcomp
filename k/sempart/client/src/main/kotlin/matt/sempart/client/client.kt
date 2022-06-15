6fa54a4fad5f037517ba6bd3d67b41dc85251b2f
mgroth0 <mgroth49@gmail.com>
2022-06-15 19:07:00 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/client.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/client.kt
@@ -53,6 +53,7 @@ fun main() = defaultMain {
 		width = 80.percent
 		position = absolute
 		left = 10.percent
+		transform = ""
 	  }
 	  oninput = {
 		document.body!!.sty.transform = "scale(${value})"
