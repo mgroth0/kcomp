8b38d208fc07b731dd9e9554b7a547006b9d14a9
mgroth0 <mgroth49@gmail.com>
2022-06-16 00:01:54 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/scaleDiv/scaleDiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/scaleDiv/scaleDiv.kt
@@ -1,8 +1,9 @@
 package matt.sempart.client.scaleDiv
 
 //import matt.sempart.client.mainDivClass
-import matt.kjs.css.TextAlign.center
-import matt.kjs.css.VerticalAligns
+import matt.kjs.css.AlignItems
+import matt.kjs.css.Display.flex
+import matt.kjs.css.JustifyContent.center
 import matt.kjs.css.auto
 import matt.kjs.css.percent
 import matt.kjs.css.sty
@@ -22,14 +23,17 @@ val scaleDiv by lazy {
 	//	classList.add(mainDivClass)
 	onlyShowIn(Scaling)
 	sty {
-	  textAlign = center
-	  verticalAlign = VerticalAligns.middle
+	  display = flex
+	  justifyContent = center
+	  alignItems = AlignItems.center
+	  //	  textAlign = center
+	  //	  verticalAlign = VerticalAligns.middle
 	  height = 100.percent
 	}
 
 	img {
 	  src = INSTRUCTIONS_IM_RELATIVE
-	  sty.verticalAlign = VerticalAligns.middle
+	  //	  sty.verticalAlign = VerticalAligns.middle
 	}
 	br
 	p {
