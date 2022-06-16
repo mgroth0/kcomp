b78dd02cc1609648b073dd73d134368f139c3560
mgroth0 <mgroth49@gmail.com>
2022-06-15 21:40:33 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/trialdiv/trialdiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/trialdiv/trialdiv.kt
@@ -122,6 +122,8 @@ private fun DrawingTrial.trialDiv(): TrialDiv = object: AwesomeElement<HTMLDivEl
   val eventCanvasIDK = stackDiv.canvas {
 	canvasConfig(3)
 	onclick = interaction("click") {
+	  println("click.clientX = ${it.clientX}")
+	  println("click.clientY = ${it.clientY}")
 	  select(eventToSeg(it))
 	}
 	setOnMouseMove {
