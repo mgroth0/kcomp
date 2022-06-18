d941135260f3ea35b1685d355ae5de17c2c9f9a2
mgroth0 <mgroth49@gmail.com>
2022-06-18 17:10:16 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/instructionsDiv/instructionsVid/instructionsVid.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/instructionsDiv/instructionsVid/instructionsVid.kt
@@ -5,6 +5,7 @@ import matt.kjs.css.auto
 import matt.kjs.css.percent
 import matt.kjs.css.sty
 import matt.kjs.handlers.setOnClick
+import matt.kjs.html.elements.button.HTMLButtonWrapper
 import matt.sempart.client.const.INSTRUCTIONS_VID_WOLFRAM
 import matt.sempart.client.state.ExperimentPhase.InstructionsVid
 import matt.sempart.client.state.ExperimentState
@@ -14,24 +15,36 @@ val instructionsVidDiv = ImageAndControlsScreen(InstructionsVid) {
   stackDiv.sty {
 	display = flex
   }
+  var playPauseButton: HTMLButtonWrapper? = null
+  var moveOnButton: HTMLButtonWrapper? = null
   val vid = stackDiv.video {
 	sty.width = 100.percent
 	source {
 	  src = INSTRUCTIONS_VID_WOLFRAM
 	  type = "video/mp4"
 	}
+	setOnEnded {
+	  playPauseButton!!.disabled = false
+	  moveOnButton!!.disabled = false
+	}
   }
-  controlsDiv.button {
-	sty.margin = auto
+  controlsDiv.sty {
+	display = flex
+  }
+  playPauseButton = controlsDiv.button {
+	sty.width = 100.percent
+	disabled = true
 	+"play/pause"
 	setOnClick {
 	  if (vid.paused) vid.play()
 	  else vid.pause()
 	}
   }
-  controlsDiv.button {
+  moveOnButton = controlsDiv.button {
+	sty.width = 100.percent
+	disabled = true
 	sty.margin = auto
-	+"Click here when ready to move on"
+	+"Click here when you are ready to move on"
 	setOnClick { ExperimentState.finishedVid.value = true }
   }
 }
