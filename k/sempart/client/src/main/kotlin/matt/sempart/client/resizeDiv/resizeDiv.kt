e044131f4e9faed968800179bdeba430dd18b8c9
mgroth0 <mgroth49@gmail.com>
2022-06-18 00:23:15 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/resizeDiv/resizeDiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/resizeDiv/resizeDiv.kt
@@ -2,6 +2,8 @@ package matt.sempart.client.resizeDiv
 
 import matt.kjs.css.auto
 import matt.kjs.css.sty
+import matt.sempart.client.const.HEIGHT
+import matt.sempart.client.const.WIDTH
 import matt.sempart.client.state.ExperimentPhase.Resize
 import matt.sempart.client.ui.ExperimentScreen
 
@@ -9,6 +11,6 @@ import matt.sempart.client.ui.ExperimentScreen
 val resizeDiv = ExperimentScreen(Resize) {
   p {
 	sty.margin = auto
-	+"Your window is too small. Please enlarge the browser window so it is at least matt.sempart.client.const.getWidth=900 by matt.sempart.client.const.getHeight=750."
+	+"Your window is too small. Please enlarge the browser window so the width is at least $WIDTH pixels and the height is at least $HEIGHT pixels."
   }
 }
