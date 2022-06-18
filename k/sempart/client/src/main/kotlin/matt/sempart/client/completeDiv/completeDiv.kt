71c953c09ec49485a05835c3d9c8370612f6cfd4
mgroth0 <mgroth49@gmail.com>
2022-06-18 18:23:17 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/completeDiv/completeDiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/completeDiv/completeDiv.kt
@@ -1,5 +1,10 @@
 package matt.sempart.client.completeDiv
 
+import matt.kjs.css.Display.flex
+import matt.kjs.css.FlexDirection.column
+import matt.kjs.css.JustifyContent.center
+import matt.kjs.css.TextAlign
+import matt.kjs.css.sty
 import matt.kjs.handlers.setOnClick
 import matt.kjs.handlers.setOnInput
 import matt.kjs.html.elements.button.HTMLButtonWrapper
@@ -8,44 +13,62 @@ import matt.sempart.Feedback
 import matt.sempart.client.const.COMPLETION_URL
 import matt.sempart.client.state.ExperimentPhase.Complete
 import matt.sempart.client.state.sendData
+import matt.sempart.client.sty.MED_SPACE
+import matt.sempart.client.sty.box
 import matt.sempart.client.ui.ExperimentScreen
 
 val completeDiv = ExperimentScreen(Complete) {
 
+  sty {
+	display = flex
+	flexDirection = column
+	justifyContent = center
+	textAlign = TextAlign.center
+  }
 
   +"The experiment is complete. Thank you for your participation!"
 
-  br
-  br
-
   var b: HTMLButtonWrapper? = null
 
-  +"Optionally, if you would like to give the researchers feedback on this experiment please submit it here."
-  val ta = textArea {
-	setOnInput {
-	  b!!.enabled = true
+  div {
+	sty {
+	  box()
+	  display = flex
+	  flexDirection = column
+	  justifyContent = center
+	  textAlign = TextAlign.center
+	  margin = MED_SPACE
 	}
-  }
-
-  val sentFeedback = VarProp(false)
-
-  span {
-	b = button {
-	  +"Submit Feedback"
-	  enabled = false
-	  setOnClick {
-		enabled = false
-		sendData(Feedback(ta.value))
+	+"Optionally, if you would like to give the researchers feedback on this experiment please submit it here."
+	val ta = textArea {
+	  setOnInput {
+		b!!.enabled = true
 	  }
 	}
-	sentFeedback.onChangeUntil({ it }, {
-	  +"Feedback received. Thank you!"
-	})
 
+	val sentFeedback = VarProp(false)
+
+	span {
+	  b = button {
+		+"Submit Feedback"
+		enabled = false
+		setOnClick {
+		  enabled = false
+		  sendData(Feedback(ta.value))
+		}
+	  }
+	  sentFeedback.onChangeUntil({ it }, {
+		+"Feedback received. Thank you!"
+	  })
+
+	}
   }
 
 
+
+
   span {
+	sty.margin = MED_SPACE
 	+"To confirm your completion of the study with Prolific (which necessary for payment) please "
 	a {
 	  href = COMPLETION_URL
