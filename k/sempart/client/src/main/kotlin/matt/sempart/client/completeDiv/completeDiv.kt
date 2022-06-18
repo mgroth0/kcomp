b2c86b7552f99ab6e6918d3edb080e02cad62a1c
mgroth0 <mgroth49@gmail.com>
2022-06-18 15:53:59 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/completeDiv/completeDiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/completeDiv/completeDiv.kt
@@ -3,13 +3,12 @@ package matt.sempart.client.completeDiv
 import matt.kjs.handlers.setOnClick
 import matt.kjs.handlers.setOnInput
 import matt.kjs.html.elements.button.HTMLButtonWrapper
+import matt.kjs.prop.VarProp
 import matt.sempart.Feedback
 import matt.sempart.client.const.COMPLETION_URL
 import matt.sempart.client.state.ExperimentPhase.Complete
 import matt.sempart.client.state.sendData
 import matt.sempart.client.ui.ExperimentScreen
-
-var debugButton: HTMLButtonWrapper? = null
 
 val completeDiv = ExperimentScreen(Complete) {
 
@@ -23,29 +22,36 @@ val completeDiv = ExperimentScreen(Complete) {
 
   +"Optionally, if you would like to give the researchers feedback on this experiment please submit it here."
   val ta = textArea {
-	println("configuring textArea")
 	setOnInput {
-	  println("in value changed")
 	  b!!.enabled = true
-	  println("b!!.enabled=${b!!.enabled}")
 	}
-	println("configured textArea")
-  }
-  b = button {
-	+"Submit Feedback"
-	enabled = false
-	setOnClick {
-	  enabled = false
-	  sendData(Feedback(ta.value))
-	}
-  }
-  debugButton = b
-  b.id = "debugButton"
-  +"To confirm your completion of the study with Prolific (which necessary for payment) please "
-  a {
-	href = COMPLETION_URL
-	+"click here"
   }
 
+  val sentFeedback = VarProp(false)
+
+  span {
+	b = button {
+	  +"Submit Feedback"
+	  enabled = false
+	  setOnClick {
+		enabled = false
+		sendData(Feedback(ta.value))
+	  }
+	}
+	sentFeedback.onChangeUntil({ it }, {
+	  +"Feedback received. Thank you!"
+	})
+
+  }
+
+
+  span {
+	+"To confirm your completion of the study with Prolific (which necessary for payment) please "
+	a {
+	  href = COMPLETION_URL
+	  +"click here"
+	}
+	+"."
+  }
 
 }
