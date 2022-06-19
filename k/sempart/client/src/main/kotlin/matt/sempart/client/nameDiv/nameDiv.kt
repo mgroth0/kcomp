059b74399993c04395f544f108148148e51114a0
mgroth0 <mgroth49@gmail.com>
2022-06-18 21:15:55 -0400

    autocommit

--- k/sempart/client/src/main/kotlin/matt/sempart/client/nameDiv/nameDiv.kt
+++ k/sempart/client/src/main/kotlin/matt/sempart/client/nameDiv/nameDiv.kt
@@ -2,7 +2,7 @@ package matt.sempart.client.nameDiv
 
 import matt.kjs.css.Display.flex
 import matt.kjs.css.FlexDirection.column
-import matt.kjs.css.JustifyContent.spaceBetween
+import matt.kjs.css.JustifyContent.center
 import matt.kjs.css.sty
 import matt.kjs.handlers.setOnClick
 import matt.kjs.handlers.setOnInput
@@ -10,22 +10,25 @@ import matt.kjs.html.elements.button.HTMLButtonWrapper
 import matt.sempart.client.state.ExperimentPhase.Name
 import matt.sempart.client.state.ExperimentState
 import matt.sempart.client.state.Participant
+import matt.sempart.client.sty.MED_SPACE
 import matt.sempart.client.ui.ExperimentScreen
 
 val nameDiv = ExperimentScreen(Name) {
   sty {
 	display = flex
 	flexDirection = column
-	justifyContent = spaceBetween
+	justifyContent = center
   }
   +"Please input your name:"
   var b: HTMLButtonWrapper? = null
   val t = textInput {
+	sty.margin = MED_SPACE
 	setOnInput {
 	  b!!.disabled = value.isNotBlank()
 	}
   }
   b = button {
+	sty.margin = MED_SPACE
 	+"Confirm Name"
 	disabled = true
 	setOnClick {
