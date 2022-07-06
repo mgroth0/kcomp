e2750518e445de5fc081fa18a7ff7b97924f8672
mgroth0 <mgroth49@gmail.com>
2022-07-06 17:53:47 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 import matt.file.toMFile
 import matt.kbuild.gbuild.depinfo.setupDepInfoTask
-import matt.kbuild.gbuild.rootDirM
 import matt.kbuild.root.addAtypicalTasksToAllProjects
 import matt.kbuild.root.checkVersionsAndProperties
 import matt.kbuild.root.configureIdeaExcludes
-import matt.kbuild.root.mustRunAfterAllCleans
 import matt.kbuild.root.setAllProjectsVersionsToGroupAndSysTime
 import matt.kbuild.root.standardizeSubprojectGroupNamesAndNames
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val thisFile = buildscript.sourceFile!!.toMFile()
 require(thisFile.hardLinkCount == 2)
 plugins {
-  id("com.github.johnrengelman.shadow") version "7.1.2"
+  id("com.github.johnrengelman.shadow") // version "7.1.2"
   id("com.dorongold.task-tree") version "2.1.0"
   /*need to reference absolute path here because gradle API in KJG doesn't switch to the right working dir*/
   val stupidKtVersion = File("/Users/matthewgroth/registered/ide/flow/RootFiles/libs.versions.toml").readText()
@@ -19,39 +17,15 @@ plugins {
   kotlin("plugin.serialization") version stupidKtVersion apply false
   idea
 }
-
 val ktversion = tomlVersion("kotlin")
 logger.info("using gradle version ${gradle.gradleVersion}")
-val tomlGradleVersion = tomlVersion("gradle")
-tasks.wrapper {/*Use Gradle from: "gradle-wrapper.properties' file*/
-  doFirst {
-	(rootDirM + "gradle" + "wrapper").listFiles()!!.forEach { it.deleteRecursively() } /*yup, necessary*/
-	(rootDirM  + "gradlew").delete()
-	(rootDirM  + "gradlew.bat").delete()
-  }
-  distributionType = Wrapper.DistributionType.ALL
-  logger.info("root wrapper gradle version set to $tomlGradleVersion")
-  gradleVersion = tomlGradleVersion
-}
+val root = matt.kbuild.root.RootProject(rootProject)
+root.configureWrapper()
 checkVersionsAndProperties()
 standardizeSubprojectGroupNamesAndNames()
 setAllProjectsVersionsToGroupAndSysTime(except = "idea")
-//allprojects {
-//  apply<IdeaPlugin>()
-//  configure<org.gradle.plugins.ide.idea.model.IdeaModel> {
-//	module {
-//		this.iml {
-//
-//		}
-//	}
-//  }
-//}
-
-
-
 addAtypicalTasksToAllProjects()
 allprojects { configureIdeaExcludes() }
-val root = matt.kbuild.root.RootProject(rootProject)
 root.addTypicalGitTasksToAppropriateProjects()
 root.addRootFilesSyncAndCompleteGitTasks()
 val allChecks = root.addCheckTasksToAllProjects()
@@ -60,17 +34,4 @@ root.tryToSilenceNPM()
 setupDepInfoTask()
 root.setupKBuildTask(allChecks)
 root.tryToFixCleanBug(allChecks)
-
-/*https://youtrack.jetbrains.com/issue/KT-50848/KotlinJS-inner-build-routines-are-using-vulnerable-NPM-dependencies-and-now-that-we-have-kotlin-js-store-github-audit-this*/
-rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin::class.java) {
-  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().lockFileDirectory =
-	rootProject.projectDir
-}
-
-
-
-allprojects {
-  tasks.withType<KotlinCompile>().all {
-	mustRunAfterAllCleans()
-  }
-}
+root.moveYarnLock()
