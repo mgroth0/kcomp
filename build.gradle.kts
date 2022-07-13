ba2658c4bd543503990d33853d1ddbabcae3c6a9
mgroth0 <mgroth49@gmail.com>
2022-07-13 15:50:36 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,21 @@ import matt.kbuild.root.checkVersionsAndProperties
 import matt.kbuild.root.configureIdeaExcludes
 import matt.kbuild.root.setAllProjectsVersionsToGroupAndSysTime
 import matt.kbuild.root.standardizeSubprojectGroupNamesAndNames
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 
 val thisFile = buildscript.sourceFile!!.toMFile()
 require(thisFile.hardLinkCount == 2)
 plugins {
-
+  //  id("com.github.johnrengelman.shadow") version "7.1.2"
   id("com.dorongold.task-tree") version "2.1.0"
+  id("com.github.johnrengelman.shadow")
 
-//  val stupidKtVersion = File("/Users/matthewgroth/registered/ide/flow/RootFiles/libs.versions.toml").readText()
-//	.substringAfter("kotlin").substringAfter('"').substringBefore('"')
+  //  val stupidKtVersion = File("/Users/matthewgroth/registered/ide/flow/RootFiles/libs.versions.toml").readText()
+  //	.substringAfter("kotlin").substringAfter('"').substringBefore('"')
   kotlin("plugin.serialization") version libs.versions.kotlin apply false
 }
 pluginManager.apply {
-  apply("com.github.johnrengelman.shadow") // version "7.1.2"
+//  apply("com.github.johnrengelman.shadow") // version "7.1.2"
   //  apply("com.dorongold.task-tree") /*version "2.1.0"*/
   /*need to reference absolute path here because gradle API in KJG doesn't switch to the right working dir*/
 
