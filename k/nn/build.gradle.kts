34b6a744b9e92bdcbdaa534b73c1c9cef643bbfd
mgroth0 <mgroth49@gmail.com>
2022-07-13 22:28:59 -0400

    autocommit

--- k/nn/build.gradle.kts
+++ k/nn/build.gradle.kts
@@ -9,20 +9,21 @@ implementations {
   async
   kjlibGit
 }
+implementations(
+  when (matt.klib.commons.thisMachine) {
+    is NEW_MAC -> fileTree((matt.file.commons.JAR_FOLDER + "tf").apply {
+      val needIn = "-${libs.tensorflow.get().versionConstraint}-SNAPSHOT"
+      require(listFiles()!!
+        .filter { it !is DSStoreFile }
+        .all { needIn in it.name }) {
+        "this check is to ensure that I'm using the same tf version on all machines"
+      }
+    })
+
+    else       -> libs.tensorflow
+  },
+  "com.github.dhruvrajan:tensorflow-keras-java:782e9cc1b622f35ca04e824eb27b1d9aa48dec6f"
+)
 compileOnlys {
   libs.tensorflow /*for navigating sources in IDE*/
 }
-implementations(
-  when (matt.klib.commons.thisMachine) {
-	is NEW_MAC -> fileTree((matt.file.commons.JAR_FOLDER + "tf").apply {
-	  val needIn = "-${libs.tensorflow.get().versionConstraint}-SNAPSHOT"
-	  require(listFiles()!!
-		.filter { it !is DSStoreFile }
-		.all { needIn in it.name }) {
-		"this check is to ensure that I'm using the same tf version on all machines"
-	  }
-	})
-
-	else       -> libs.tensorflow
-  },
-)
--- k/nn/src/main/kotlin/matt/nn/mnistselectivity/mnistselectivity.kt
+++ k/nn/src/main/kotlin/matt/nn/mnistselectivity/mnistselectivity.kt
@@ -2,7 +2,6 @@ package matt.nn.mnistselectivity
 
 import matt.async.date.tic
 import matt.file.commons.DATA_FOLDER
-import matt.kjlib.shell.shell
 
 /*ORIG: https://colab.research.google.com/drive/1n1yBIF3rSA5KPqP8VYk97yLZ7VSK37qe#scrollTo=y5a20t84B01h*/
 /*MINE (just added download line): https://colab.research.google.com/drive/1vGnaYrjaNdGTiPZ2msZDJ-WEzyNdlBWb*/
@@ -15,14 +14,19 @@ fun mnistSelectivityDemo() {
   val sampleDataFold = PRIV_DATA_FOLD + "sample_data"
 
   val robustnessFold = PRIV_DATA_FOLD + "Robustness"
-  sampleDataFold.mkdirs()
-  if (robustnessFold.doesNotExist) {
-	t.toc("cloning robustness")
-	shell("git", "clone", "https://github.com/kimvc7/Robustness.git", workingDir = PRIV_DATA_FOLD, debug = true)
-	t.toc("finished cloning robustness")
-  }
-  t.toc("generating datasets")
-  shell("/usr/bin/python3", "main.py", "--run=config", "--config=generate_datasets", workingDir = robustnessFold)
-  t.toc("finished generating datasets")
+//  sampleDataFold.mkdirs()
+//  if (robustnessFold.doesNotExist) {
+//	t.toc("cloning robustness")
+//	shell("git", "clone", "https://github.com/kimvc7/Robustness.git", workingDir = PRIV_DATA_FOLD, debug = true)
+//	t.toc("finished cloning robustness")
+//  }
+//  t.toc("generating datasets")
+//  shell("/usr/bin/python3", "main.py", "--run=config", "--config=generate_datasets", workingDir = robustnessFold)
+//  t.toc("finished generating datasets")
+
+
+  MNIST()
+
+
   t.toc("finished MNIST selectivity demo")
 }
