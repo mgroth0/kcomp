7f476788f6141bc61ef8d975a53180b3f1f06d0b
mgroth0 <mgroth49@gmail.com>
2022-07-15 14:54:49 -0400

    autocommit

--- k/nn/keras/src/main/java/org/tensorflow/keras/activations/Activation.java
+++ k/nn/keras/src/main/java/org/tensorflow/keras/activations/Activation.java
@@ -1,7 +1,7 @@
 package org.tensorflow.keras.activations;
 
 import org.tensorflow.Operand;
-import org.tensorflow.Shape;
+import org.tensorflow.op.core.Shape;
 import org.tensorflow.keras.layers.Layer;
 import org.tensorflow.keras.mixin.ActivationFunction;
 import org.tensorflow.op.Ops;
--- k/nn/keras/src/main/java/org/tensorflow/keras/layers/Layer.java
+++ k/nn/keras/src/main/java/org/tensorflow/keras/layers/Layer.java
@@ -1,7 +1,7 @@
 package org.tensorflow.keras.layers;
 
 import org.tensorflow.Operand;
-import org.tensorflow.Shape;
+import org.tensorflow.op.core.Shape;
 import org.tensorflow.keras.initializers.Initializer;
 import org.tensorflow.keras.initializers.Initializers;
 import org.tensorflow.keras.mixin.LayerFunction;
