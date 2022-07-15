92c1605539afa12801301cd97976760e154f9e48
mgroth0 <mgroth49@gmail.com>
2022-07-15 16:37:34 -0400

    autocommit

--- k/nn/keras/src/main/java/org/tensorflow/keras/metrics/Accuracy.java
+++ k/nn/keras/src/main/java/org/tensorflow/keras/metrics/Accuracy.java
@@ -1,5 +1,6 @@
 package org.tensorflow.keras.metrics;
 
+import kotlin.NotImplementedError;
 import org.tensorflow.Operand;
 import org.tensorflow.op.core.Shape;
 import org.tensorflow.op.Ops;
@@ -8,11 +9,15 @@ import org.tensorflow.types.TInt64;
 import org.tensorflow.types.family.TNumber;
 
 public class Accuracy extends Metric {
-  @Override
-  public <T extends TNumber> Operand<T> apply(Ops tf, Class<T> dtype, Operand<T> output, Operand<T> label) {
-    Operand<TInt64> predicted = tf.argMax(output, tf.constant(1));
-    Operand<TInt64> expected = tf.argMax(label, tf.constant(1));
+    @Override
+    public <T extends TNumber> Operand<T> apply(Ops tf, Class<T> dtype, Operand<T> output, Operand<T> label) {
+        Operand<TInt64> predicted = tf.math.argMax(output, tf.constant(1));
+        Operand<TInt64> expected = tf.math.argMax(label, tf.constant(1));
 
-    return tf.mean(tf.cast(tf.equal(predicted, expected), dtype), tf.constant(0));
-  }
+
+        throw new NotImplementedError("""
+                tf.math.mean(tf.cast(predicted.equals(expected), dtype), tf.constant(0));        
+                        """);
+
+    }
 }
