e7897594932eae938034d83d46d12c5a0f4391de
mgroth0 <mgroth49@gmail.com>
2022-07-15 14:25:07 -0400

    autocommit

--- k/nn/build.gradle.kts
+++ k/nn/build.gradle.kts
@@ -9,11 +9,9 @@ implementations {
 }
 implementations(
   projects.k.nn.keras,
-
   "org.jetbrains.kotlinx:kotlin-deeplearning-api:0.4.0",
   "org.jetbrains.kotlinx:kotlin-deeplearning-onnx:0.4.0",
   "org.jetbrains.kotlinx:kotlin-deeplearning-visualization:0.4.0"
-
 )
 compileOnlys {
   libs.tensorflow /*for navigating sources in IDE*/
