ffc435fe0a6a2e969bc597ddffa28b5ba8bb75bb
mgroth0 <mgroth49@gmail.com>
2022-07-17 15:08:47 -0400

    autocommit

--- k/nn/build.gradle.kts
+++ k/nn/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import matt.kbuild.root.mod.JavaExecutable
 import matt.key.REMOTE_GRADLE_ARG
 import matt.key.REMOTE_SHADOW_ARG
 
@@ -18,10 +19,14 @@ implementations(
 
 tasks {
   val remoteGradleRun by registering(JavaExec::class) {
+	classpath = sourceSets.main.get().runtimeClasspath
+	mainClass.set(JavaExecutable(project).mainClass)
 	dependsOn(rootProject.tasks.matching { it.name == matt.kbuild.root.KBUILD_TASK_NAME })
 	args!!.add(REMOTE_GRADLE_ARG)
   }
   val remoteShadowRun by registering(JavaExec::class) {
+	classpath = sourceSets.main.get().runtimeClasspath
+	mainClass.set(JavaExecutable(project).mainClass)
 	dependsOn(withType<ShadowJar>())
 	args!!.add(REMOTE_SHADOW_ARG)
   }
