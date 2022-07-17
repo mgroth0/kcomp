3ce791e4528641cb9939d1f9f8945219b9008e44
mgroth0 <mgroth49@gmail.com>
2022-07-17 16:55:19 -0400

    autocommit

--- k/nn/README.txt
+++ k/nn/README.txt
@@ -1,9 +1,4 @@
-this currently doesnt work for other users because files need to be writable
-
-I will probably need to create a shadow jar if other users want to execute
-
 cd /om2/user/mjgroth/kcomp
 module load openmind/singularity
-singularity exec -B /om2/user/mjgroth/kcomp:/om2/user/mjgroth/kcomp:ro -B /om2/user/mjgroth/data:/om2/user/mjgroth/data:ro -H /om2/user/mjgroth --nv kcomp.simg /bin/bash
-cd /om2/user/mjgroth/kcomp
-/opt/gradle/gradle-7.5-rc-4/bin/gradle k:nn:run --console=plain
+singularity exec -B /om2/user/mjgroth/data:/om2/user/mjgroth/data:ro -H /om2/user/mjgroth --nv kcomp.simg /bin/bash
+java -jar /om2/user/mjgroth/registered/bin/jar/nn.jar 3 # <- number of epochs
--- k/nn/build.gradle.kts
+++ k/nn/build.gradle.kts
@@ -18,33 +18,30 @@ tasks {
   //	dependsOn(rootProject.tasks.matching { it.name == matt.kbuild.root.KBUILD_TASK_NAME })
   //	args = listOf(REMOTE_GRADLE_ARG)
   //  }
+
+  val OMMachine = matt.remote.openmind.Polestar
+
   val remoteGradleRun by registering {
 	dependsOn(rootProject.tasks.matching { it.name == matt.kbuild.root.KBUILD_TASK_NAME })
 	doLast {
-	  val OMMachine = matt.remote.openmind.Polestar
-	  val srun = if (OMMachine != matt.remote.openmind.Polestar) matt.remote.slurm.SRun(timeMin = 15) else null
 	  OMMachine.ssh {
-		runOnOM(matt.remote.GradleTaskExec("k:nn:run"), srun = srun)
+		runOnOM(matt.remote.GradleTaskExec("k:nn:run"), srun = OMMachine.standardSRun)
 	  }
 	}
   }
   val remoteShadowJar by registering {
 	dependsOn(rootProject.tasks.matching { it.name == matt.kbuild.root.KBUILD_TASK_NAME })
 	doLast {
-	  val OMMachine = matt.remote.openmind.Polestar
-	  val srun = if (OMMachine != matt.remote.openmind.Polestar) matt.remote.slurm.SRun(timeMin = 15) else null
 	  OMMachine.ssh {
-		runOnOM(matt.remote.GradleTaskExec("k:nn:shadowJar"), srun = srun)
+		runOnOM(matt.remote.GradleTaskExec("k:nn:shadowJar"), srun = OMMachine.standardSRun)
 	  }
 	}
   }
   val remoteShadowRun by registering {
 	dependsOn(withType<ShadowJar>())
 	doLast {
-	  val OMMachine = matt.remote.openmind.Polestar
-	  val srun = if (OMMachine != matt.remote.openmind.Polestar) matt.remote.slurm.SRun(timeMin = 15) else null
 	  OMMachine.ssh {
-		runOnOM(matt.remote.ShadowJarExec("nn", args = listOf("4")), srun = srun)
+		runOnOM(matt.remote.ShadowJarExec("nn", args = listOf("4")), srun = OMMachine.standardSRun)
 	  }
 	}
   }
