ddb8234c532983d8f9b803cae8f60fa8e00af90d
mgroth0 <mgroth49@gmail.com>
2022-07-17 14:39:14 -0400

    autocommit

--- k/nn/build.gradle.kts
+++ k/nn/build.gradle.kts
@@ -10,3 +10,11 @@ implementations(
   libs.kotlindl.api,
   libs.bundles.kotlindl.gpu
 )
+
+tasks {
+  val remoteGradleRun by registering(JavaExec::class) {
+	dependsOn(rootProject.tasks.matching { it.name == "kbuild" })
+	args!!.add("REMOTE_GRADLE")
+  }
+}
+
