ab1f420edf3838f9744b1e22dd7367ddb861b15c
mgroth0 <mgroth49@gmail.com>
2022-07-17 19:42:08 -0400

    autocommit

--- k/v1/build.gradle.kts
+++ k/v1/build.gradle.kts
@@ -1,27 +1,25 @@
-
-implementations{
+implementations {
   mstruct
+  gui
+  fxGraphics
+  caching
+  async
+  remote
+  remoteSlurm
+  remoteOpenmind
+  fxlibConsole
+  dataman
+  kjlibJmath
+  reflect
+  fxGraphics
+  file
+  libs.apply {
+	`kotlinx-multik-api`
+	mat
+	`commons-math`
+	chartFX
+	apfloat
+	`fx-swing`
+	`slf4j-nop`
+  }
 }
-
-implementations(
-  projects.k.gui,
-  libs.kotlinx.multik.api,
-  libs.mat,
-  projects.k.fx.graphics,
-  projects.k.caching,
-  projects.k.async,
-  libs.commons.math,
-  libs.chartFX,
-  libs.apfloat,
-  libs.fx.swing,
-  projects.k.remote,
-  projects.k.remote.slurm,
-  projects.k.remote.openmind,
-  projects.k.fxlib.console,
-  projects.k.dataman,
-  projects.k.kjlib.jmath,
-  projects.k.reflect,
-  projects.k.fx.graphics,
-  libs.slf4j.nop,
-  ":k:file".jvm(),
-)
