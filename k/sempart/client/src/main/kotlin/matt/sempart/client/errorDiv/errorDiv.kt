1e9ee1cd9b5303b40aac599a414205d0a1b496c1
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-18 12:03:46 -0400

    remove k/sempart which is to become submodule

--- k/sempart/client/src/main/kotlin/matt/sempart/client/errorDiv/errorDiv.kt
+++
@@ -1,8 +0,0 @@
-package matt.sempart.client.errorDiv
-
-import matt.sempart.client.state.ExperimentPhase.Err
-import matt.sempart.client.ui.ExperimentScreen
-
-val errorDiv = ExperimentScreen(
-  Err
-)
--- k/sempart/client/src/main/kotlin/matt/sempart/client/inactiveDiv/inactiveDiv.kt
+++
@@ -1,8 +0,0 @@
-package matt.sempart.client.inactiveDiv
-
-import matt.sempart.client.state.ExperimentPhase.Inactive
-import matt.sempart.client.ui.ExperimentScreen
-
-val inactiveDiv = ExperimentScreen(Inactive) {
-  element.innerHTML = "Sorry, you have been inactive for too long and the experiment has been cancelled."
-}
